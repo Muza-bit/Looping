d161636f7d97ef411f53eedec6eaca9491545d85
Muza-bit <182216332+Muza-bit@users.noreply.github.com>
2024-10-09 17:51:24 +0700

    Add files via upload

---
+++ Loop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    cout << "Masukkan jumlah data : ";
+    cin >> n;
+
+    int jumlahGanjil = 0;
+    int jumlahGenap = 0;
+
+    for (int i = 1; i < n; ++i) {
+      if (i % 2 == 0) {
+        jumlahGenap += i;
+      } else {
+        jumlahGanjil += i;
+      }
+    }
+        cout << "Jumlah total bilangan genap : " << jumlahGenap << endl;
+        cout << "Jumlah total bilangan ganjil : " << jumlahGanjil << endl;
+        return 0;
+}
